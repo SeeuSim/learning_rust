de4d788b79a07bd414b4bc0d207767113ede8366
SeeuSim <seeusimong@gmail.com>
2023-08-20 19:21:13 +0800

    [Chp 2] Complete guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,25 +8,41 @@ fn main() {
 
     println!("{}----------------", "Guess the number\n");
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess: ");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line"); //IMPORTANT
-                                        //If missing, the Result type may
-                                        //  become an Error
+    loop {
+        println!("Please input your guess: ");
     
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); //IMPORTANT
+                                            //If missing, the Result type may
+                                            //  become an Error
+        
+        // expect => Crash with this error
+        // match => Provide case by case handling of error (ok, error)
 
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // let guess:u32 = guess.trim().parse().expect("A number was not input");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Oops! Expected a positive integer, but another string was input.");
+                continue;
+            },
+        };
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
+
 }
