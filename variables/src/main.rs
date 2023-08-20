2f94a8cdcb7c9d4e26155bc6bf75f00cc7988112
SeeuSim <seeusimong@gmail.com>
2023-08-20 23:38:56 +0800

    [Chp 3] Add shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,5 @@
 fn mutability() {
+    println!("======= MUTABILITY =========");
     // let x = 5;
     // Without `mut`, variables may be treated as `const`
     // Therefore, they may not be reassigned without `mut`
@@ -19,8 +20,42 @@ fn variables() {
     // NOTE: Only in the scope which they are defined (incl global)
 }
 
+fn shadowing() {
+    println!("======= SHADOWING =========");
+
+    // #1 Same types, re-assignment
+
+    //first instantiation
+    let x = 5;
+
+    // Second instantiation OVER the first
+    // >> FOR the rest of the program, it will refer to this one.
+    let x = x + 1;
+
+    {
+        // Third one - only in THIS scope.
+        let x = x * 2;
+        println!("The value of x in the inner scope is {x}");
+    }
+
+    println!("The value of x in the inner scope is {x}");
+
+    // #2 Type Switching :)
+
+    // This is ok.
+    let _spaces = "    ";
+    let _spaces = _spaces.len(); // `free` the earlier reference, and point it to this type.
+
+    // This is not.
+    // let mut _spaces = "     ";
+    // _spaces = _spaces.len(); // This attempts to store a variable of a different type without
+    //     `free`-ing the reference first. (`mut` reference)
+}
+
 fn main() {
     mutability();
 
     variables();
+
+    shadowing();
 }
